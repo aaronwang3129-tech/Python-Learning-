3332ded878304405cf6aead34b4f6f652f9194c4
aaronwang3129-tech <aaronwang3129@gmail.com>
2025-12-30 14:13:12 -0700

    Delete Notes/Weight_Converter.py

--- Notes/Weight_Converter.py
+++
@@ -1,8 +0,0 @@
-weight = input("Weight: ")
-unit = input("(K)g or (L)bs: ")
-if unit.upper() == "L":
-    weightk = float(weight)/2.205
-    print("Your weight in Kg: " + str(weightk))
-elif unit.upper() == "K":
-    weightl = float(weight)*2.205
-    print("Your weight in Lb " + str(weightl))
